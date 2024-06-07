0a1c7c522940ebd0390329dc4e38331bcb0aaae3
Prajwal P <prajwalp264@gmail.com>
2024-06-07 23:35:41 +0530

    feat: identity reconciliation with typeorm - mysql

---
+++ src/entity/Contact.ts
@@ -0,0 +1,39 @@
+import { Entity, PrimaryGeneratedColumn, Column, Index } from "typeorm";
+
+/*
+ * Table: Contact
+ *
+ * id Int
+ * phoneNumber String?
+ * email String?
+ * linkedId Int? // the ID of another Contact linked to this one
+ * linkPrecedence "secondary"|"primary" // "primary" if it's the first Contact in th
+ * createdAt DateTime
+ * 
+ */
+@Entity()
+@Index("emailIndex", ["email", "linkPrecedence", "createdAt"], { sparse: true })
+@Index("phoneNumberIndex", ["phoneNumber", "linkPrecedence", "createdAt"], { sparse: true })
+export class Contact {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: true })
+  phoneNumber: string | null;
+
+  @Column({ nullable: true })
+  email: string | null;
+
+  @Index({ sparse: true })
+  @Column({ nullable: true })
+  linkedId: number | null;
+
+  @Column({})
+  linkPrecedence: string;
+
+  @Column({ type: 'timestamp' })
+  createdAt: Date;
+
+  @Column({type: "timestamp" })
+  updatedAt: Date;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,123 @@
-import express, { json, NextFunction, Request, Response } from "express";
+import express, { json, Request, Response } from "express";
 import "reflect-metadata";
-import { createConnection } from "typeorm";
-import { User } from "./entity/User";
+import { createConnection, Connection, Not, In } from "typeorm";
+import { Contact } from "./entity/Contact";
+
+interface IIdentityReconciliationRequest {
+  email?: null | string,
+  phoneNumber?: null | string
+}
+
+interface IConsolidatedContactResponse {
+  primaryContatctId: number,
+  emails: string[],
+  phoneNumbers: string[],
+  secondaryContactIds: number[],
+}
+
+const findContacts = async (conn: Connection, payload: IIdentityReconciliationRequest) => {
+  const conditons = [];
+  if (payload.phoneNumber) {
+    conditons.push({ phoneNumber: payload.phoneNumber })
+  }
+  if (payload.email) {
+    conditons.push({ email: payload.email })
+  }
+  let contacts = await conn.manager.find(Contact, {
+    where: conditons,
+    order: {
+      linkPrecedence: 'ASC',
+      createdAt: 'DESC'
+    },
+  });
+  if (conditons.length === 1 && contacts.length) {
+    const linkedContactsConditions: any[] = [{
+      linkedId: contacts[0].linkedId || contacts[0].id,
+    }];
+    if (contacts[0].linkedId) {
+      linkedContactsConditions.push({
+        id: contacts[0].linkedId,
+      });
+    }
+    const linkedContacts = await conn.manager.find(Contact, {
+      where: linkedContactsConditions
+    });
+    contacts.push(...linkedContacts.filter((item) => (!contacts.find((i) => i.id === item.id))));
+    if (linkedContactsConditions.length === 2) {
+      contacts = contacts.sort((a, b) => a.linkPrecedence.localeCompare(b.linkPrecedence))
+    }
+  }
+  return contacts;
+}
+
+const createContact = (conn: Connection, payload: IIdentityReconciliationRequest, linkPrecedence = 'primary', linkedId?: number) => {
+  const contact = new Contact();
+  contact.phoneNumber = payload.phoneNumber || null;
+  contact.email = payload.email || null;
+  contact.linkPrecedence = linkPrecedence;
+  contact.linkedId = linkedId || null;
+  contact.createdAt = new Date();
+  contact.updatedAt = new Date();
+  return conn.manager.save(contact)
+}
+
+const consolidateContactResponse = (contacts: Contact[]) => {
+  if (contacts.length === 1) {
+    const emails: string[] = []
+    const phoneNumbers: string[] = []
+    if (contacts[0].email) {
+      emails.push(contacts[0].email);
+    }
+    if (contacts[0].phoneNumber) {
+      phoneNumbers.push(contacts[0].phoneNumber);
+    }
+    return {
+      primaryContatctId: contacts[0].id,
+      emails,
+      phoneNumbers,
+      secondaryContactIds: []
+    }
+  }
+  const consolidatedResponse = {
+    emails: [],
+    phoneNumbers: [],
+    secondaryContactIds: []
+  } as IConsolidatedContactResponse;
+  for (const contact of contacts) {
+    if (!consolidatedResponse.primaryContatctId) {
+      consolidatedResponse.primaryContatctId = contact.id;
+    }
+    if (!consolidatedResponse.emails.includes(contact.email) && contact.email) {
+      consolidatedResponse.emails.push(contact.email);
+    }
+    if (!consolidatedResponse.phoneNumbers.includes(contact.phoneNumber) && contact.phoneNumber) {
+      consolidatedResponse.phoneNumbers.push(contact.phoneNumber);
+    }
+    if (contact.linkPrecedence !== 'primary') {
+      consolidatedResponse.secondaryContactIds.push(contact.id);
+    }
+  }
+  return consolidatedResponse;
+}
+
+const doesContactExist = (payload: IIdentityReconciliationRequest, contacts: Contact[]): Contact | undefined => {
+  return contacts.find((contact) => contact.phoneNumber === payload.phoneNumber && contact.email === payload.email);
+}
+
+const checkIfPayloadHasNewInfo = (payload: IIdentityReconciliationRequest, contacts: Contact[]) => {
+  let doesEmailMatch = false;
+  let doesPhoneNumberMatch = false;
+  for (const contact of contacts) {
+    if (contact.email === payload.email) {
+      doesEmailMatch = true;
+    }
+    if (contact.phoneNumber == payload.phoneNumber) {
+      doesPhoneNumberMatch = true;
+    }
+  }
+  let hasNewInfo = !doesEmailMatch || !doesPhoneNumberMatch;
+  return hasNewInfo
+}
 
 createConnection()
   .then(async (connection) => {
@@ -10,41 +126,59 @@ createConnection()
     app.use(json());
 
     app.post(
-      "/user",
+      "/identify",
       async (
         req: Request,
         res: Response,
-        next: NextFunction
       ): Promise<void> => {
-        const user = new User();
-        user.firstName = req.body.firstName;
-        user.lastName = req.body.lastName;
-        user.age = req.body.age;
-
-        console.log("Inserting a new user into the database...");
-        await connection.manager.save(user);
-        console.log("Saved a new user with id: " + user.id);
-
-        res.status(200).json({ userId: user.id });
-
-        next();
-      }
-    );
-
-    app.get(
-      "/user",
-      async (
-        req: Request,
-        res: Response,
-        next: NextFunction
-      ): Promise<void> => {
-        console.log("Loading users from the database...");
-        const users = await connection.manager.find(User);
-        console.log("Loaded users: ", users);
-
-        res.json(users);
-
-        next();
+        const payload = req.body as IIdentityReconciliationRequest;
+        // Check if we can find any data with the email | phone number
+        const contacts = await findContacts(connection, payload);
+        // if we don't find the contacts we will create one
+        if (!payload.email || !payload.phoneNumber) {
+          const consolidatedContact = consolidateContactResponse(contacts);
+          res.status(200).json({ contact: consolidatedContact });
+          return;
+        }
+        if (!contacts?.length) {
+          const contact = await createContact(connection, payload);
+          const consolidatedContact = consolidateContactResponse([contact]);
+          res.status(200).json({
+            contact: consolidatedContact
+          });
+          return;
+        }
+        // contact already exist
+        const exactContact = doesContactExist(payload, contacts);
+        if (!exactContact) {
+          // check if the new record is having new information
+          const hasNewInfo = checkIfPayloadHasNewInfo(payload, contacts);
+          if (hasNewInfo) {
+            const contact = await createContact(connection, payload, 'secondary', contacts[0].id);
+            contacts.push(contact);
+          } else {
+            // take the oldest one as primary and set others as secondary
+            contacts.sort((a, b) => +new Date(b.createdAt) - +new Date(a.createdAt));
+            let index = 0;
+            for (let contact of contacts) {
+              if (!index) {
+                contact.linkedId = null;
+                contact.linkPrecedence = 'primary';
+              } else {
+                contact.linkedId = contacts[0].id;
+                contact.linkPrecedence = 'secondary';
+              }
+              await connection.createQueryBuilder()
+                .update(Contact)
+                .set(contact)
+                .where({ id: contact.id })
+                .execute();
+              index++;
+            }
+          }
+        }
+        const consolidatedContact = consolidateContactResponse(contacts);
+        res.status(200).json({ contact: consolidatedContact });
       }
     );
 
